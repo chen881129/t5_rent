4d1ffabe7bf5723b8cf80ae6a4be92449b25cc05
chen881129 <chen9479.happy@163.com>
2018-06-30 22:03:15 +0800

    add listen port into argv

--- main.py
+++ main.py
@@ -4,10 +4,14 @@
 from BaseHTTPServer import HTTPServer
 from handler.ToDoHandler import TodoHandler
 import MySQLdb
+import sys
 from conf import *
 
 def main():
-    server = HTTPServer(('0.0.0.0', LISTEN_PORT), TodoHandler)
+    port = LISTEN_PORT
+    if (len(sys.argv) == 2):
+        port = int(sys.argv[1])
+    server = HTTPServer(('0.0.0.0', port), TodoHandler)
     print("Starting server, use <Ctrl-C> to stop")
     server.serve_forever()
 
