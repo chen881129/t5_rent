798df39fa98f47cf9c8ad10d7b51b464cddc02ec
chen881129 <chen9479.happy@163.com>
2018-07-01 08:38:21 +0800

    add autotest

---
+++ autotest.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+function wait_port()
+{
+    port=$1
+    pid=$2
+    while 1
+    do
+        ps -p $pid
+        if [[ $? -ne 0 ]];then
+            return 1
+        fi
+        listen=`netstat -nalp | grep "\<$port\>"`
+        if [[ $? -eq 0 ]];then
+            return 0
+        fi
+    done
+}
+
+echo "start main server"
+sudo python main.py 1010 &>test_log &
+PID=$!
+wait_port 1010 $PID
+if [[ $? -ne 0 ]];then
+    echo "start server failed"
+    exit 1
+fi
+
+kill $PID
